872cf1e6f0b119c3344397b25c0ef8493696e768
xuetong <3292752511@qq.com>
2023-05-17 22:14:46 +0800

    git commit 2

--- src/main/java/com/xt/test/GitTest.java
+++ src/main/java/com/xt/test/GitTest.java
@@ -3,5 +3,6 @@ package com.xt.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 1111");
+        System.out.println("hello git 2222");
     }
 }
