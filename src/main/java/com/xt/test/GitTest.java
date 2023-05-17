2487f7e3044811fba580cecfc211837f9acd52fa
xue996007 <130204540+xue996007@users.noreply.github.com>
2023-05-17 23:00:48 +0800

    pull commit

--- src/main/java/com/xt/test/GitTest.java
+++ src/main/java/com/xt/test/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git 4444");
         System.out.println("hello git master 6666666");
         System.out.println("push test");
+        System.out.println("pull test")
     }
 }
